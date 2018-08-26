d3de848bfe17a047cf67e64eac5059293d9d8a2f
Caydn12 <Caydnlofton@gmail.com>
2018-08-26 10:31:35 -0700

    Added a function to show how to pass a map and iterate over a map.

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// Creates a structure where [keys] are of type string, and values are of type string
 	colors := map[string]string{
@@ -12,14 +8,19 @@ func main() {
 		"green": "#00FF00",
 	}
 
-	var emptyMap map[int]string
+	//var emptyMap map[int]string
 
 	otherColorMap := make(map[string]string)
 	otherColorMap["white"] = "#FFFFFF"
 
 	delete(colors, "red")
 
-	fmt.Println(colors)
-	fmt.Println(emptyMap)
-	fmt.Println(otherColorMap)
+	PrintMap(colors)
+	PrintMap(otherColorMap)
+}
+
+func PrintMap(c map[string]string) {
+	for color, hex := range c {
+		println("Key: " + color + " | Value: " + hex)
+	}
 }
